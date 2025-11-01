2665ee150d02ca65d32d0639654505f6e160eb12
Bhawana83 <bhawanasahani11@gmail.com>
2025-11-01 14:33:30 +0530

    Updated server.js file

--- backend/server.js
+++ backend/server.js
@@ -46,10 +46,7 @@ app.use("/api/admin", adminRoutes);
 // Connect to MongoDB
 const PORT = process.env.PORT || 5000;
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_URI, {})
 
   .then(() =>
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
